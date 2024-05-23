2b66ec5db7ae31040318893472af4638a3f56222
DonaldChen1 <donald@orderly.network>
2024-05-23 10:33:04 +0800

    feat(analyzer) add brokers

---
+++ orderly-dashboard-analyzer/migrations/2024-05-23-023204_add_brokers/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+insert into broker_info(broker_id,broker_hash) values
+('ascendex','0x9a5106cc3ebaab2273ad6428bd17dc5b9bfef504b8b6d7057d4680a802df14b1'),
+('sharpe_ai','0x9921dea59df194f068703edf9f7ab023a0996dbc4508b1112874ae7ae203dfe1'),
+('vooi','0x8aad03786175b7bc33c13f1d0274d9fe1100d3ce8f7cca7c3b76419b90b1d4a4'),
+('fusionx_pro','0x02395383525c02a08430a77ab8c0e9d7976a27dbd72e5be7b35096d05b11668d'),
+('elixir','0xe22e67d157b3bc31fee7d2b433dd478f3753b70f70ba3aaba2587da4d2d9b73e ');-- Your SQL goes here
