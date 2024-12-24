078c768fb0f340499b458a3946412109879cc6dc
Spring <yongchun@orderly.network>
2024-12-24 16:46:07 +0800

    feat-qs: direct calculate accountid in memory

--- orderly-dashboard-query-service/src/events/events_api.rs
+++ orderly-dashboard-query-service/src/events/events_api.rs
@@ -1,15 +1,11 @@
-use actix_diesel::dsl::AsyncRunQueryDsl;
 use actix_web::{get, web, HttpResponse, Responder};
 use chrono::{Duration, Utc};
-use diesel::{ExpressionMethods, QueryDsl};
 use serde_derive::Deserialize;
 
 use crate::config::get_common_cfg;
 use orderly_dashboard_analyzer::db::user_info::UserInfo;
-use orderly_dashboard_analyzer::db::POOL;
-use orderly_dashboard_analyzer::schema::user_info::dsl::*;
 use orderly_dashboard_indexer::formats_external::trading_events::AccountTradingEventsResponse;
-use orderly_dashboard_indexer::formats_external::{FailureResponse, Response, SuccessResponse};
+use orderly_dashboard_indexer::formats_external::{FailureResponse, Response};
 
 pub(crate) const QUERY_ACCOUNT_EVENT_CONTEXT: &str = "query_account_event_context";
 
@@ -38,70 +34,45 @@ pub async fn list_events(
     param: web::Query<GetAccountEventsRequest>,
 ) -> actix_web::Result<impl Responder> {
     tracing::info!(target: QUERY_ACCOUNT_EVENT_CONTEXT, "query account events params: {:?}", param);
-    let mut select_result: Result<UserInfo, _> = user_info
-        .filter(address.eq(param.address.clone()))
-        .filter(broker_id.eq(param.broker_id.clone()))
-        .first_async::<UserInfo>(&POOL)
-        .await;
-    if select_result.is_err() {
-        match UserInfo::try_new(param.broker_id.clone(), param.address.clone()) {
-            Err(err) => {
-                let resp = FailureResponse::new(
-                    1000,
-                    format!("parse account_id failed with err: {}", err),
-                );
-                return Ok(HttpResponse::Ok().json(resp));
-            }
-            Ok(user_info_) => {
-                select_result = Ok(user_info_);
-            }
+    let user_info_res = match UserInfo::try_new(param.broker_id.clone(), param.address.clone()) {
+        Ok(user_info_res) => user_info_res,
+        Err(err) => {
+            let resp =
+                FailureResponse::new(1000, format!("parse account_id failed with err: {}", err));
+            return Ok(HttpResponse::Ok().json(resp));
         }
-    }
+    };
 
-    match select_result {
-        Ok(user_info_res) => {
-            let indexer_data = get_indexer_data(
-                param.from_time,
-                param.to_time,
-                user_info_res.account_id,
-                param.event_type.as_deref().map(str::to_uppercase),
-                get_common_cfg().indexer_address.clone(),
-            )
-            .await;
+    let indexer_data = get_indexer_data(
+        param.from_time,
+        param.to_time,
+        user_info_res.account_id,
+        param.event_type.as_deref().map(str::to_uppercase),
+        get_common_cfg().indexer_address.clone(),
+    )
+    .await;
 
-            match indexer_data {
-                Ok(res_json_str) => {
-                    let result: Result<Response<AccountTradingEventsResponse>, serde_json::Error> =
-                        serde_json::from_str(&*res_json_str);
+    match indexer_data {
+        Ok(res_json_str) => {
+            let result: Result<Response<AccountTradingEventsResponse>, serde_json::Error> =
+                serde_json::from_str(&*res_json_str);
 
-                    match result {
-                        Ok(response) => {
-                            return Ok(HttpResponse::Ok().json(response));
-                        }
-                        Err(err) => {
-                            let resp = FailureResponse::new(
-                                1000,
-                                format!(
-                                    "get_indexer_data parse event_type failed with err: {}",
-                                    err
-                                ),
-                            );
-                            return Ok(HttpResponse::Ok().json(resp));
-                        }
-                    }
+            match result {
+                Ok(response) => {
+                    return Ok(HttpResponse::Ok().json(response));
                 }
                 Err(err) => {
                     let resp = FailureResponse::new(
                         1000,
-                        format!("get user info failed with err: {}", err),
+                        format!("get_indexer_data parse event_type failed with err: {}", err),
                     );
                     return Ok(HttpResponse::Ok().json(resp));
                 }
-            };
+            }
         }
-        Err(_) => {
-            let resp: Response<AccountTradingEventsResponse> =
-                Response::Success(SuccessResponse::new(AccountTradingEventsResponse::default()));
+        Err(err) => {
+            let resp =
+                FailureResponse::new(1000, format!("get user info failed with err: {}", err));
             return Ok(HttpResponse::Ok().json(resp));
         }
     };
