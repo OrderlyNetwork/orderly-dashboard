4062bd5d080e6b66ce02b2404e4b360600caf3e9
Mario Reder <mario.reder@pm.me>
2025-08-21 10:46:33 +0200

    fix: remove copy tx hash

--- orderly-dashboard-FE/app/routes/address/TransactionLink.tsx
+++ orderly-dashboard-FE/app/routes/address/TransactionLink.tsx
@@ -1,11 +1,9 @@
 import { FC } from 'react';
 
-import { Shortened } from './Shortened';
-
-export const TransactionLink: FC<{ value?: number | string; displayCount?: number }> = ({
-  value,
-  displayCount
-}) => {
+export const TransactionLink: FC<{
+  value?: number | string;
+  displayCount?: number;
+}> = ({ value, displayCount = 6 }) => {
   if (value == null) return null;
 
   const txId = String(value);
@@ -13,7 +11,9 @@ export const TransactionLink: FC<{ value?: number | string; displayCount?: numbe
 
   return (
     <a href={explorerUrl} target="_blank" rel="noopener noreferrer" className="hover:underline">
-      <Shortened value={txId} displayCount={displayCount} />
+      <span>
+        {txId.substring(0, displayCount)}...{txId.substr(-4)}
+      </span>
     </a>
   );
 };
