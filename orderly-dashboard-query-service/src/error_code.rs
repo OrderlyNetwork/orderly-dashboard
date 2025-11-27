c383aa67a1011d48952ff4308d2dedbd9b47890e
Spring <yongchun@orderly.network>
2025-11-27 18:12:35 +0800

    feat: add check param of token deposit/withdraw ranking

--- orderly-dashboard-query-service/src/error_code.rs
+++ orderly-dashboard-query-service/src/error_code.rs
@@ -6,6 +6,8 @@ pub const QUERY_OVER_LIMIT_ERR: i32 = 10004;
 pub const QUERY_OVER_EXECUTION_ERR: i32 = 10005;
 pub const ACCOUNT_ID_CONFLICT_OR_INVALID_ERR: i32 = 10006;
 pub const ACCOUNT_NOT_FOUND: i32 = 10007;
+pub const DAYS_RAGE_OVER_LIMIT: i32 = 10008;
+pub const SIZE_OVER_LIMIT: i32 = 10009;
 
 pub const RAW_QUERY_OVERLIMIT_ERR_MESSAGE: &str =
     "too many raw query in parallel, please try later";
@@ -13,3 +15,5 @@ pub const RAW_QUERY_EXECUTE_ERR_MESSAGE: &str = "raw query execute failed";
 pub const RAW_QUERY_BAN_ERR_MESSAGE: &str = "raw query is not open";
 pub const ACCOUNT_ID_CONFLICT_OR_INVALID_ERR_MESSAGE: &str =
     "account_id and <address, broker_id> should not appear in one request not invalid";
+pub const DAYS_RAGE_OVER_LIMIT_ERR_MESSAGE: &str = "days_rage_over_limit";
+pub const SIZE_OVER_LIMIT_ERR_MESSAGE: &str = "size_over_limit";
