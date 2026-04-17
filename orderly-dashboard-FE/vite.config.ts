c8722c63b5e7c5fe55a09e1110bd490641985728
Mario Reder <mario.reder@pm.me>
2026-04-17 09:27:22 +0200

    fix: remove vercel related packages

--- orderly-dashboard-FE/vite.config.ts
+++ orderly-dashboard-FE/vite.config.ts
@@ -1,10 +1,9 @@
 import { vitePlugin as remix } from '@remix-run/dev';
-import { vercelPreset } from '@vercel/remix/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths()],
   ssr: {
     noExternal: process.env.NODE_ENV === 'production' ? ['@mui/**/*'] : undefined
   }
